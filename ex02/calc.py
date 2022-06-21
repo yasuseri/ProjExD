05509424422500fb78ad17766f2990013271a266
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-06-21 14:35:34 +0900

    電卓２

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo("", f"{num}のボタンがクリックされました")
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x500") #フィールドを生成
@@ -9,12 +14,14 @@ if __name__ == "__main__":
     r, c= 0,0   #列番号と行番号
     for num in range(9,-1,-1):
         btn = tk.Button(root, text=f"{num}", width=4, height=2,
-             font=("Time New Roman", 30)) #9のボタンを生成
+             font=("Time New Roman", 30)) #それぞれのボタンを生成
 
+        btn.bind("<1>",button_click)
         btn.grid(row=r, column=c)
 
-        c+=1
+        c += 1
         if (num-1)%3 == 0:
-            r+=1
-            c=0
+            r += 1
+            c = 0
+
     root.mainloop()
