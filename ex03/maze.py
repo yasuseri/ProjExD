6d8ccd71e2408cdde43109e3200467b373c67d4b
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-06-28 14:24:00 +0900

    練習２

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,3 +5,11 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     root.mainloop()
+
+    canvas = tk.Canvas(root, width=1500, height=900, bg="balack")
+    canvas.pack()
+
+    root.mainloop()
+
+
+
