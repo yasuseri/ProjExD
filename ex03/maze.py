4d0e5c7f6af4a1e2f3e199a690add0547a41ee2c
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-06-28 15:13:22 +0900

    練習８

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,47 +1,51 @@
 import tkinter as tk
+import maze_maker as mm
 
 def key_down(event):
-    global key
+    global key   #グローバル変数
     key = event.keysym
-    #print(f"{key}キーが押されました")
 
 def key_up(event):
-    global key
+    global key   #グローバル変数
     key = ""
 
 def main_proc():
-    global cx, cy
-    delta = {   #キー：押されているキーkey/値：移動幅リスト[x,y]
-            ""      : [0,   0],
-            "Up"    : [0, -20], 
-            "Down"  : [0, +20],
-            "Left"  : [-20, 0],
-            "Right" : [+20, 0],
-            }
-    cx, cy = cx+delta[key][0], cy+delta[key][1]
-    #if key == "Up":cy -=20
+    global cx, cy   #グローバル変数
+    #delta = {   #キー：押されているキーkey/値：移動幅リスト[x,y]
+    #        ""      : [0,   0],
+    #        "Up"    : [0, -20], 
+    #        "Down"  : [0, +20],
+    #        "Left"  : [-20, 0],
+    #        "Right" : [+20, 0],
+    #        }
+    #cx, cy = cx+delta[key][0], cy+delta[key][1]
+    if key == "Up"    : cy-=20
+    if key == "Down"  : cy+=20
+    if key == "Left"  : cx-=20
+    if key == "Right" : cx+=20
+    if key == ""      : cx+=0
     
     canvas.coords("tori", cx, cy)
     root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるこうかとん")   #タイトルを書く
 
-    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")   #背景色とウインドウを生成
     canvas.pack()
 
-    tori = tk.PhotoImage(file="fig/6.png")
-    cx, cy = 300, 400
+    tori = tk.PhotoImage(file="fig/6.png")   #６のこうかとんを使う
+    cx, cy = 300, 400  #初期位置
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
 
-    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyPress>", key_down)   #キーが押されたら…
     root.bind("<KeyRelease>", key_up)
 
     main_proc()
-    root.mainloop()
+    root.mainloop()  #ウインドウを表示
 
 
 
