516dace155b36a331805ea05637a9d044fe629ed
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-06-28 16:57:16 +0900

    ４限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import maze_maker as mm
 
+
 def key_down(event):
     global key   #グローバル変数
     key = event.keysym
@@ -9,6 +10,7 @@ def key_up(event):
     global key   #グローバル変数
     key = ""
 
+
 def main_proc():
     global cx, cy, mx, my   #グローバル変数
     delta = {   #キー：押されているキーkey/値：移動幅リスト[x,y]
@@ -16,22 +18,28 @@ def main_proc():
             "Down"  : [0, +1],
             "Left"  : [-1, 0],
             "Right" : [+1, 0],
+            "a"     : [-1, 0],
+            "w"     : [0, -1],
+            "s"     : [0, +1],
+            "d"     : [+1, 0],
     }
     try:
         if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:  #もし移動先が床なら
             my, mx = my+delta[key][1], mx+delta[key][0]
-    except:
+    except:  #もし移動先が壁なら
         pass
 
-    #mx+delta[key][0], my+delta[key][1]
-    #if maze_bg[mx+delta[key][0]][my+delta[key][1]] == 1:  #もし移動先が床なら
-    #    my, mx = my+delta[key][1], mx+delta[key][0]
-    #if key == "Up"    : my-=1
-    #if key == "Down"  : my+=1
-    #if key == "Left"  : mx-=1
-    #if key == "Right" : mx+=1
     cx, cy = mx*100+50, my*100+50
     canvas.coords("tori", cx, cy)
+    if cx == 1350:
+        if cy == 750:
+            tori = tk.PhotoImage(file="fig/3.png")
+            mx, my = 1, 1
+            cx, cy = mx*100+50, my*100+50
+            canvas.create_image(cx,cy, image=tori, tag='tori')
+            
+    else:
+        pass
     root.after(100,main_proc)
 
 if __name__ == "__main__":
@@ -40,17 +48,26 @@ if __name__ == "__main__":
 
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")   #背景色とウインドウを生成
     canvas.pack()
+
+
     maze_bg = mm.make_maze(15, 9)   #1が壁,0が床のリストを生成
     mm.show_maze(canvas, maze_bg)   #canvasにmaze_bgを書く
     #print(maze_bg)
 
-    tori = tk.PhotoImage(file="fig/6.png")   #６のこうかとんを使う
+    tori = tk.PhotoImage(file="fig/3.png")   #5のこうかとんを使う
+    st = tk.PhotoImage(file="fig/5.png")   #スタート地点にこうかとん
+    go = tk.PhotoImage(file="fig/4.png")
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50  #初期位置
     
     canvas.create_image(cx, cy, image=tori, tag="tori")
+    canvas.create_image(150, 150, image=st, tag="st")
+    canvas.create_image(1350, 750, image=go, tag="go")
+    
 
     key = ""
+
+
 
     root.bind("<KeyPress>", key_down)   #キーが押されたら…
     root.bind("<KeyRelease>", key_up)
