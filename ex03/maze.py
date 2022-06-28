0aca1567ffe75c40aa2912c1092af3b26a121e36
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-06-28 15:31:41 +0900

    練習１１

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,21 +10,20 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy   #グローバル変数
+    global cx, cy, mx, my   #グローバル変数
     #delta = {   #キー：押されているキーkey/値：移動幅リスト[x,y]
     #        ""      : [0,   0],
-    #        "Up"    : [0, -20], 
-    #        "Down"  : [0, +20],
-    #        "Left"  : [-20, 0],
-    #        "Right" : [+20, 0],
+    #        "Up"    : [0, -1], 
+    #        "Down"  : [0, +1],
+    #        "Left"  : [-1, 0],
+    #        "Right" : [+1, 0],
     #        }
     #cx, cy = cx+delta[key][0], cy+delta[key][1]
-    if key == "Up"    : cy-=20
-    if key == "Down"  : cy+=20
-    if key == "Left"  : cx-=20
-    if key == "Right" : cx+=20
-    if key == ""      : cx+=0
-    
+    if key == "Up"    : my-=1
+    if key == "Down"  : my+=1
+    if key == "Left"  : mx-=1
+    if key == "Right" : mx+=1
+    cx, cy = mx*100+50, my*100+50
     canvas.coords("tori", cx, cy)
     root.after(100,main_proc)
 
@@ -39,7 +38,9 @@ if __name__ == "__main__":
     #print(maze_bg)
 
     tori = tk.PhotoImage(file="fig/6.png")   #６のこうかとんを使う
-    cx, cy = 300, 400  #初期位置
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50  #初期位置
+    
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
