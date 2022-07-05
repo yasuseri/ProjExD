29d8cc8156cd2f0f17faa2b716273b422813765a
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-07-05 14:59:40 +0900

    練習５、５

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,24 +1,35 @@
 import pygame as pg
 import sys
+import random
 
 def main():
-    #練習１
+    #練習１　スクリーン
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ!!こうかとん")
-    screen_sft = pg.display.set_mode((1600, 900))  #Surface
-    sreen_rct = screen_sft.get_rect()              #Rect
-    bgimg_sft = pg.image.load("fig/pg_bg.jpg")     #Surface
-    bgimg_rct = bgimg_sft.get_rect()               #Rect
-    screen_sft.blit(bgimg_sft, bgimg_rct)
+    screen_sfc = pg.display.set_mode((1600, 900))  #Surface
+    screen_rct = screen_sfc.get_rect()              #Rect
+    bgimg_sfc = pg.image.load("fig/pg_bg.jpg")     #Surface
+    bgimg_rct = bgimg_sfc.get_rect()               #Rect
+    screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-    #練習3
+    #練習3　：こうかとん
     kkimg_sft = pg.image.load("fig/6.png")         #Surface
     kkimg_sft = pg.transform.rotozoom(kkimg_sft, 0, 2.0)       #Surface
     kkimg_rct = kkimg_sft.get_rect()               #Rect
     kkimg_rct.center = 900, 400      #こうかとん表示
 
+    #練習5　：爆弾
+    bmimg_sfc = pg.Surface((20,20)) #Surface
+    bmimg_sfc.set_colorkey((0, 0, 0))  #黒い部分を透明
+    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
+    bmimg_rct = bmimg_sfc.get_rect() #Rect
+    bmimg_rct.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct.centery = random.randint(0, screen_rct.height)
+
+    vx, vy = +1, +1 #練習6
+
     while True:
-        screen_sft.blit(bgimg_sft, bgimg_rct)  #Surfaceに貼り付け
+        screen_sfc.blit(bgimg_sfc, bgimg_rct)  #Surfaceに貼り付け
         
         #練習2
         for event in pg.event.get():
@@ -30,10 +41,15 @@ def main():
         if key_states[pg.K_DOWN]  == True: kkimg_rct.centery +=1
         if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -=1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx +=1
+        screen_sfc.blit(kkimg_sft, kkimg_rct)  #Surfaceに貼り付け
 
-        screen_sft.blit(kkimg_sft, kkimg_rct)  #Surfaceに貼り付け
+        #練習5
+        screen_sfc.blit(bmimg_sfc, bmimg_rct)
 
-        pg.display.update()
+        #練習6
+        bmimg_rct.move_ip(vx, vy)
+
+        pg.display.update()  #描画を更新
         clock.tick(1000)
 
 if __name__ == "__main__":
