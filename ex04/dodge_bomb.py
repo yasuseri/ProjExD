eaa87fc5003607fa7768a2642c26438b0ee19407
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-07-05 15:36:57 +0900

    練習7

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -41,6 +41,13 @@ def main():
         if key_states[pg.K_DOWN]  == True: kkimg_rct.centery +=1
         if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -=1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx +=1
+
+        if check_bound(kkimg_rct, screen_rct) != (1,1): #領域外ならば
+            if key_states[pg.K_UP]    == True: kkimg_rct.centery +=1
+            if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -=1
+            if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx +=1
+            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -=1
+
         screen_sfc.blit(kkimg_sft, kkimg_rct)  #Surfaceに貼り付け
 
         #練習5
@@ -48,9 +55,19 @@ def main():
 
         #練習6
         bmimg_rct.move_ip(vx, vy)
-
+        #練習7
+        yoko, tate = check_bound(bmimg_rct, screen_rct)
+        vx *= yoko
+        vy *= tate
         pg.display.update()  #描画を更新
         clock.tick(1000)
+
+#練習7
+def check_bound(rct, scr_rct):   #rctはこうかとんと爆弾のRect、scr_rctはスクリーンのRect
+    yoko, tate = +1, +1
+    if rct.left < scr_rct.left or scr_rct.right  < rct.right  : yoko = -1 #領域外
+    if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom : tate = -1 #領域外
+    return yoko, tate 
 
 if __name__ == "__main__":
     pg.init()
