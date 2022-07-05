842ab34585ddad0b542880f47f550600cb771851
serizawa <c0a21074c4@edu.teu.ac.jp>
2022-07-05 14:24:51 +0900

    練習３

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,17 +2,24 @@ import pygame as pg
 import sys
 
 def main():
+    #練習１
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ!!こうかとん")
-    screen_sft = pg.display.set_mode((1600, 900))  #surface
-    sreen_rct = screen_sft.get_rect()              #rect
-    bgimg_sft = pg.image.load("fig/pg_bg.jpg")      #surface
-    bgimg_rct = bgimg_sft.get_rect()               #rect
+    screen_sft = pg.display.set_mode((1600, 900))  #Surface
+    sreen_rct = screen_sft.get_rect()              #Rect
+    bgimg_sft = pg.image.load("fig/pg_bg.jpg")     #Surface
+    bgimg_rct = bgimg_sft.get_rect()               #Rect
     screen_sft.blit(bgimg_sft, bgimg_rct)
 
+    #練習２
+    kkimg_sft = pg.image.load("fig/6.png")         #Surface
+    kkimg_sft = pg.transform.rotozoom(kkimg_sft, 0, 2.0)       #Surface
+    kkimg_rct = kkimg_sft.get_rect()               #Rect
+    kkimg_rct.center = 900, 400      #こうかとん表示
 
     while True:
         screen_sft.blit(bgimg_sft, bgimg_rct)
+        screen_sft.blit(kkimg_sft, kkimg_rct)
 
         #練習
         for event in pg.event.get():
